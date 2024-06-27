63a41066e98e4ae9cdab9d375c38e325e182338a
Yesler2113 <diazmunozdm12@gmail.com>
2024-06-27 11:14:01 -0600

    Prueba

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import { Screen1 } from './presentation/screens/Screnn1';
+
+export const App = () => {
+  return (
+    <View>
+      <Screen1 />
+    </View>
+  );
+};
+
+export default App;
---
+++ src/presentation/screens/Screnn1.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from 'react-native';
+
+export const Screen1 = () => {
+  return (
+    <View>
+      <Text>Esta es una prueba para el examen</Text>
+    </View>
+  );
+};
